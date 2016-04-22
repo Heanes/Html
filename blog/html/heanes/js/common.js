b455b7b3616bf06a29a4ba89b02a959405ea1074
fanggang <fanggang@meicai.cn>
2016-04-22 22:08:19 +0800

    iframe 优化

--- blog/html/heanes/js/common.js
+++ blog/html/heanes/js/common.js
@@ -6,18 +6,34 @@
 
 
 /**
- * @doc iframe自适应高度
- * @param fm_name 框架名称
- * @param fm_id 框架id
+ * @doc 原生js使iframe自适应宽度高度
+ * @use 用法,例如<iframe onLoad="frameAutoHeight('FM_header','FM_header')" src="../layout/header.html" name="FM_header" id="FM_header"></iframe>
+ * @param frameName 框架名称
+ * @param frameId 框架id
  * @author fanggang
  * @time 2016-02-22 20:24:11 周一
  */
-function frameAutoHeight(fm_name, fm_id){
-    var frm = document.getElementById(fm_id);
-    var subWeb = document.frames ? document.frames[fm_name].document : frm.contentDocument;
-    if(frm != null && subWeb != null){
-        frm.style.height = subWeb.documentElement.scrollHeight + "px";
+function frameAutoHeight(frameName, frameId){
+    var frame = document.getElementById(frameId);
+    var subWeb = document.frames ? document.frames[frameName].document : frame.contentDocument;
+    if(frame != null && subWeb != null){
+        frame.style.height = subWeb.documentElement.scrollHeight + "px";
         //如需自适应宽高，去除下行的“//”注释即可
-        frm.style.width = subWeb.documentElement.scrollWidth + "px";
+        frame.style.width = subWeb.documentElement.scrollWidth + "px";
     }
 }
+
+/**
+ * @doc 使iframe自适应宽度高度
+ * @author fanggang
+ * @time 2016-04-22 20:04:10
+ */
+$(function () {
+    $('iframe').on('load', function () {
+        var thisHeight = $(this).contents().height();
+        $(this).css('height', thisHeight);
+        var thisWidth = $(this).contents().width();
+        $(this).css('width', thisWidth);
+    });
+
+});
