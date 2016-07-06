8b9a00c6fbbfe33444003a31c5b9b49a27f6af39
Heanes <heanes@163.com>
2016-07-06 12:17:02 +0800

    admin moudle update

--- admin/html/modernUI/js/layout.js
+++ admin/html/modernUI/js/layout.js
@@ -6,6 +6,8 @@ $(function () {
     var $menuTopUl = $('#menuTopUl');
     var $menuTopList = $menuTopUl.find('li');
     var $menuLeftFamily = $('.menu-left-family');
+    var $menuLeftGroup = $('.menu-left-group');
+    var $menuLeftGroupLiList = $menuLeftGroup.find('li');
     $menuTopList.each(function (i, item) {
         $(item).on('click', function () {
             $menuTopList.removeClass('active');
@@ -13,10 +15,13 @@ $(function () {
             $($menuLeftFamily[i]).addClass('active');
             $(this).addClass('active');
         });
+        // 按顶部菜单激活侧边菜单
+        if($(item).hasClass('active')){
+            $($menuLeftFamily).removeClass('active');
+            $($menuLeftFamily[i]).addClass('active');
+        }
     });
 
-    var $menuLeftGroup = $('.menu-left-group');
-    var $menuLeftGroupLiList = $menuLeftGroup.find('li');
     $menuLeftGroupLiList.each(function (i, item) {
         if($(item).find('ul.menu-left-group').length > 0){
             $(item).find('.menu-parent').first().on('click', function () {
